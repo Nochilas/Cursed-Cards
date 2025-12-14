56d74e84282c53cc7b0d1e3456a4ef45da764fe9
Nicholas <Nicholas@NOCHILAS>
2025-12-14 19:06:49 +0100

    implemented abstract fattory pattern DDD model with containerization and refactoring of gay comments

--- CursedCardsBackend/Endpoints/EndpointConfig.cs
+++ CursedCardsBackend/Endpoints/EndpointConfig.cs
@@ -9,16 +9,35 @@ public static class EndpointConfig
         /// </summary>
         public void ConfigureEndpoints()
         {
-            app.AddCreateGameEndpoint();        // Player creates a new game
-            app.AddJoinGameEndpoint();          // Player joins an existing game
-            app.AddGetLobbyStateEndpoint();     // Returns lobby state for UI refresh
-            app.AddStartGameEndpoint();         // Czar starts a new game
-            app.AddGetGameStateEndpoint();      // Returns game state for UI refresh
-            app.AddDrawWhiteCardsEndpoint();    // Player draws a number of white cards
-            app.AddDrawBlackCardEndpoint();     // Player draws a black card
-            app.AddStartRoundEndpoint();        // Czar starts a new round
-            app.AddPlayCardsEndpoint();         // Player plays a card
-            app.AddSelectWinnerEndpoint();      // Czar selects a winner for the current round
+            // Player creates a new game
+            app.AddCreateGameEndpoint();
+
+            // Player joins an existing game
+            app.AddJoinGameEndpoint();
+
+            // Returns lobby state for UI refresh
+            app.AddGetLobbyStateEndpoint();
+
+            // Czar starts a new game
+            app.AddStartGameEndpoint();
+
+            // Returns game state for UI refresh
+            app.AddGetGameStateEndpoint();
+
+            // Player draws a number of white cards
+            app.AddDrawWhiteCardsEndpoint();
+
+            // Player draws a black card
+            app.AddDrawBlackCardEndpoint();
+
+            // Czar starts a new round
+            app.AddStartRoundEndpoint();
+
+            // Player plays a card
+            app.AddPlayCardsEndpoint();
+
+            // Czar selects a winner for the current round
+            app.AddSelectWinnerEndpoint();
         }
     } 
 }
