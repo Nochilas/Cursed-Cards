bf12028c6e25fb2b046987e459619de8522a570a
Nicholas <Nicholas@NOCHILAS>
2025-12-25 10:56:28 +0100

    fixed gameHub: immediately send status to joined client

--- CursedCardsBackend/Hubs/GameHub.cs
+++ CursedCardsBackend/Hubs/GameHub.cs
@@ -1,9 +1,26 @@
+using CursedCardsBackend.Constants;
+using CursedCardsBackend.Models;
+using CursedCardsBackend.Services;
 using Microsoft.AspNetCore.SignalR;
 
-public class GameHub : Hub
+public class GameHub(GameService gameService) : Hub
 {
-    public async Task JoinRoom(string roomId)
-        => await Groups.AddToGroupAsync(Context.ConnectionId, roomId);
+public async Task JoinRoom(string roomId)
+    {
+        await Groups.AddToGroupAsync(Context.ConnectionId, roomId);
+
+        var gameState = gameService.ReadGameState();
+        if (gameState.RoomId != roomId)
+        {
+            return;
+        }
+
+        // Immediately send status to joined client
+        await Clients.Caller
+            .SendAsync(
+                method: CursedCardsConstants.UPDATED_LOBBY_EVENT,
+                new LobbyDTO(gameState.Players, gameState.Czar, gameState.GameStarted));
+    }
 
     public async Task LeaveRoom(string roomId)
         => await Groups.RemoveFromGroupAsync(Context.ConnectionId, roomId);
